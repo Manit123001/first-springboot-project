965afd7bb8de225bb285a1b5150d2e2ef3ad0932
Manit Cholpinyo <captain.newclear@gmail.com>
2018-11-14 15:59:52 +0700

    Initial commit

---
+++ src/main/java/com/in28minutes/springboot/firstspringbootproject/FirstSpringbootProjectApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot.firstspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringbootProjectApplication.class, args);
+	}
+}
